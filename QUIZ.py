79b1ccb770888fe4b996bf502f9902863e6605cd
dinosaur-neil <105762644+dinosaur-neil@users.noreply.github.com>
2023-08-12 18:38:09 +1200

    Update QUIZ.py
    Adding buttons for selecting answer

--- QUIZ.py
+++ QUIZ.py
@@ -20,5 +20,41 @@ q = (obj ['ques'])
 options = (obj['options'])
 a = (obj ['ans'])
 
+# Creating a class for the QUIZ Q&A section
+
+class Quiz:
+    def __init__(self):
+        self.qn = 0
+        self.opt_selected = IntVar()
+        self.opts = self.Radiobtn()
+        self.ques = self.question(self.qn)
+        self.correct = 0
+        
+# Displaying the title and question for the QUIZ
+
+    def question(self, qn):
+        t = Label(root, text="UEFA CHAMPIONS LEAGUE QUIZ", width= 50, bg="blue", fg="white", font=("times",20,"bold"))   
+        t.place(x=0, y=2)
+        qn = Label(root, text=q[qn], width=60, font=("times", 16, "bold"), anchor="w")
+        qn.place(x=70,y=100)
+        return qn
+  
+
+    
+    def Radiobtn(self):
+        val = 0
+        b = []
+        yp = 150
+        while val < 4:
+            btn = Radiobutton(root, text="", variable=self.opt_selected, value=val + 1, font=("times", 14))
+            b.append(btn)
+            btn.place(x=100, y=yp)
+            val +=1
+            yp += 40
+        return b
+        
+
+
+quiz=Quiz()
 
 root.mainloop()
