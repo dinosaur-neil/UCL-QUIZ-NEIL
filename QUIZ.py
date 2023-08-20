bbf461404125c4d553c52d69f58b9813c98d0255
dinosaur-neil <105762644+dinosaur-neil@users.noreply.github.com>
2023-08-20 23:13:43 +1200

    Update QUIZ.py
    Adding timer, home page, and results page

--- QUIZ.py
+++ QUIZ.py
@@ -8,11 +8,12 @@
 from tkinter import*
 from tkinter import messagebox as mb
 import json
+import threading
 
 #Creating QUIZ window to put questions and answers
 
 root = Tk()
-root.geometry("800x500")
+root.geometry("800x475")
 root.title("Quiz")
 with open('File.json') as f:
     obj = json.load(f)
@@ -33,6 +34,26 @@ class Quiz:
         self.display_options(self.qn)
         self.buttons()
         self.correct = 0
+        self.time_left= 60
+        self.timer_label = Label(root, text="Timer: 60s", bg ="Midnight Blue", fg="white", font=("times", 14, "bold"))
+        self.timer_label.place(x=25, y=60)
+        self.start_timer()
+
+
+
+    def start_timer(self): 
+        self.timer_thread = threading.Thread(target=self.timer_countdown) 
+        self.timer_thread.start()
+
+    def timer_countdown(self):
+        while self.time_left > 0:
+            self.timer_label.config(text=f"Timer: {self.time_left}s")    
+            self.time_left -= 1
+            root.update()
+            import time
+            time.sleep(1)
+        self.display_result()    
+
         
 # Creating the title and question for the QUIZ
 
@@ -71,15 +92,19 @@ class Quiz:
             self.opts[val]['text'] = op
             val += 1
 
+      
+
 # Creating next question button and Exit quiz button
 # Next will show the next question & answers whereas Exit will close the window
 
     def buttons(self):
-        nbutton = Button(root, text="Next", command=self.nextbtn, width=10, bg="deep sky blue", fg="White", font=("times",16,"bold"))
-        nbutton.place(x=345,y=380)
-        quitbutton = Button(root, text="EXIT", command=root.destroy, width=10, bg="red2", fg="white", font=("times",16,"bold"))
-        quitbutton.place(x=665,y=45)
+        self.next_button = Button(root, text="Next", command=self.nextbtn, width=10, bg="deep sky blue", fg="White", font=("times",16,"bold"))
+        self.next_button.place(x=345,y=380)
+        quit_button = Button(root, text="EXIT", command=root.destroy, width=10, bg="red2", fg="white", font=("times",16,"bold"))
+        quit_button.place(x=665,y=45)
 
+    def check(self):
+        pass
 # This method retrieves the users entered value
 
     def checkans(self, qn):
@@ -91,29 +116,107 @@ class Quiz:
      
     def nextbtn(self):
         selected_option = self.opt_selected.get()
-        if selected_option == 0: # Check if an answer option is selected
-            mb.showerror("Error", "Please choose an answer.") # Error message
-            return  # Stay on the same question until an answer option is selected   
+        if selected_option == 0:  # Check if an option is selected
+            mb.showerror("Error", "Please choose an answer.")
+            return  # Stay on the same question until an option is selected
 
         if self.checkans(self.qn):
             self.correct += 1
         self.qn += 1
         if self.qn == len(q):
-            self.display_result
+            self.display_result()
         else:
-            self.display_options(self.qn)             
+            self.display_options(self.qn)
  
 # Display score in message box
 
     def display_result(self):
-        score = int(self.correct / len(q) * 100)
-        result = "Score" + str(score) + "%"
-        wc = len(q) - self.correct
-        correct = "No. of correct answers: " + str(self.correct)
-        wrong =  "No. of wrong answers: " + str(wc)
-        mb.showinfo("result: ", "\n".join([result, correct, wrong]))
+        self.ques.destroy()  # Destroy the question label
+        for opt in self.opts:
+            opt.destroy()  # Destroy the option checkboxes
+        self.next_button.destroy()  # Remove the "Next" button
+        self.timer_label.destroy()  # Remove the timer label
+        result_window = ResultWindow(self.correct, len(q))
+        
+
+
+# Creating home page
+
+class StartScreen:
+    def __init__(self):
+        self.title_label = Label(root, text="UEFA CHAMPIONS LEAGUE QUIZ", width=50, bg="DeepPink2", fg="white", font=("times", 20, "bold"))
+        self.title_label.place(x=0, y=2)
+
+        self.title_label = Label(root, text="Welcome to my Quiz, test your UCL knowledge!", width=40, fg="white", bg="Midnight Blue", font=("times", 20, "bold"))
+        self.title_label.place(x=70, y=140)
+
+        self.start_button = Button(root, text="START QUIZ!", bg="deep sky blue", fg="white", command=self.start_quiz, width=15, font=("times", 16, "bold"))
+        self.start_button.place(x=300, y=250)
+
+        quit_button = Button(root, text="EXIT", fg="white", bg="red2", command=root.destroy, width=10, font=("times", 16, "bold"))
+        quit_button.place(x=665, y=45)
+
+    def start_quiz(self):
+        self.title_label.destroy()
+        self.start_button.destroy()
+        quiz = Quiz()
+
+start_screen = StartScreen()
+
+
+# Creating a results window for users to go to after finishing QUIZ
+
+class ResultWindow:
+    def __init__(self, correct, total):
+        self.result_label = Label(root, text="UEFA CHAMPIONS LEAGUE QUIZ FINISHED!", width=50, bg="DeepPink2", fg="white", font=("times", 20, "bold"))
+        self.result_label.place(x=0, y=2)
+
+        score = str(correct) + "/" + str(total)
+        result = "Score: " + score
+        result_label = Label(root, text=result, bg="Midnight Blue", fg="white", font=("times", 18, "bold"))
+        result_label.place(x=345, y=200)
+
+# Display the "Congratulations" label if the score is perfect (all questions correct)
+        if correct == total:
+            congrats_label = Label(root, text="Congratulations Champ!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            congrats_label.place(x=275, y=150)
+
+        if correct == 0:
+            unlucky_label = Label(root, text="Unlucky Mate!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            unlucky_label.place(x=318, y=150)
+            
+        if correct == 1:
+            unlucky_label = Label(root, text="Nice Try Mate!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            unlucky_label.place(x=320, y=150)
+
+        if correct == 2:
+            unlucky_label = Label(root, text="Solid Try Mate!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            unlucky_label.place(x=318, y=150)
+
+        if correct == 3:
+            unlucky_label = Label(root, text="Good job Mate!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            unlucky_label.place(x=318, y=150)    
+       
+        if correct == 4:
+            unlucky_label = Label(root, text="Great Job Genius!", bg="Midnight Blue", fg="White", font=("times", 18, "bold"))
+            unlucky_label.place(x=308, y=150)
+
+        self.restart_button = Button(root, text="Restart", fg="white", bg="deep sky blue", command=self.restart_quiz, width=10, font=("times", 16, "bold"))
+        self.restart_button.place(x=340, y=380)
+
+        quit_button = Button(root, text="EXIT", fg="white", bg="red2", command=root.destroy, width=10, font=("times", 16, "bold"))
+        quit_button.place(x=665, y=45)
+
+    def on_close(self):
+        root.destroy()
+
+    def restart_quiz(self):
+        self.result_label.destroy()
+        self.restart_button.destroy()
+        for widget in root.winfo_children():
+            widget.destroy()
+        start_screen = StartScreen()
 
 
 
-quiz=Quiz()
 root.mainloop()
